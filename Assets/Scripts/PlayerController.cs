efafc5e6f09fe547d98ee05a89772d051b772eaf
Desages NATHAN <nathan.desages@gmail.com>
2019-01-26 00:57:23 +0100

    added prototype scene and simple movement script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+
+	Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+
+		Vector3 velocity = new Vector3(x, 0.0f, z).normalized * speed * Time.fixedDeltaTime;
+
+		rb.MovePosition(transform.position + velocity);
+	}
+}
