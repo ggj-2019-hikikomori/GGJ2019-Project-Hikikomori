dfb719aa7952a1dd1969c01ac6a0ca2f6b830e36
Desages NATHAN <nathan.desages@gmail.com>
2019-01-26 17:00:22 +0100

    added pickup management with animation and few tweaks

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+	public float rotateSpeed;
+
+	void FixedUpdate ()
+	{
+		transform.Rotate(new Vector3(0.0f, 1.0f * rotateSpeed * Time.fixedDeltaTime, 0.0f));
+	}
+}
