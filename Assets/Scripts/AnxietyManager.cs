351ce31a1a94d0af704bebde13dc4d5564fb086e
Desages NATHAN <nathan.desages@gmail.com>
2019-01-26 03:50:32 +0100

    added anxiety management with postprocess

---
+++ Assets/Scripts/AnxietyManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class AnxietyManager : MonoBehaviour
+{
+	[Range(0, 100)]
+	public float anxietyLevel;
+	float lastAnxietyLevel;
+
+	public PostProcessingProfile profile;
+
+	private void Start()
+	{
+		updatePostProcessingProfile();
+	}
+
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Keypad0))
+			anxietyLevel -= 0.2f;
+		else if (Input.GetKey(KeyCode.Keypad1))
+			anxietyLevel += 0.2f;
+
+		anxietyLevel = Mathf.Clamp(anxietyLevel, 0.0f, 100.0f);
+
+		if(anxietyLevel != lastAnxietyLevel)
+		{
+			lastAnxietyLevel = anxietyLevel;
+			updatePostProcessingProfile();
+		}
+	}
+	
+	void updatePostProcessingProfile()
+	{
+		ColorGradingModel.Settings colorGradingSettings = profile.colorGrading.settings;
+		colorGradingSettings.basic.saturation = (100 - anxietyLevel) / 100.0f;
+		profile.colorGrading.settings = colorGradingSettings;
+
+		if (anxietyLevel >= 25.0f)
+		{
+			Camera.main.GetComponentInChildren<CameraFollow>().setShakingValue(Mathf.Lerp(0.0f, 0.2f, Mathf.InverseLerp(25.0f, 100.0f, anxietyLevel)));
+			GrainModel.Settings grainSettings = profile.grain.settings;
+			grainSettings.intensity = Mathf.Lerp(0.0f, 1.0f, Mathf.InverseLerp(25.0f, 100.0f, anxietyLevel));
+			profile.grain.settings = grainSettings;
+		}
+		else
+		{
+			Camera.main.GetComponentInChildren<CameraFollow>().setShakingValue(0.0f);
+			GrainModel.Settings grainSettings = profile.grain.settings;
+			grainSettings.intensity = 0.0f;
+			profile.grain.settings = grainSettings;
+		}
+	}
+}
--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -9,13 +9,22 @@ public class CameraFollow : MonoBehaviour
 	public float smoothSpeed;
 	public Vector3 offset;
 
+	float shakingValue = 0.0f;
+
 	void FixedUpdate ()
 	{
 		Vector3 desiredPosition = target.position + offset;
 		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
 
-		transform.position = smoothedPosition;
+		Vector3 shakingOffset = new Vector3(Random.Range(-shakingValue, shakingValue), Random.Range(-shakingValue, shakingValue), Random.Range(-shakingValue, shakingValue));
+
+		transform.position = smoothedPosition + shakingOffset;
 
 		transform.LookAt(target);
+	}
+
+	public void setShakingValue(float value)
+	{
+		shakingValue = value;
 	}
 }
