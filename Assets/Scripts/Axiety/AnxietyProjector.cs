93c01df52aa08646b0d326d0da3fa58c4d124ade
Eva <eva.gerbert.gaillard@gmail.com>
2019-01-26 22:56:30 +0100

    Anxiety projector on Player + fix sprite

---
+++ Assets/Scripts/Axiety/AnxietyProjector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnxietyProjector : MonoBehaviour {
+
+    public float emptyPosition;
+    public float fullPosition;
+
+	void Start () {
+	}
+	
+
+	void Update () {
+        float yPosition = Mathf.Lerp(emptyPosition, fullPosition, GameManager.instance.anxietyLevel / 100);
+        transform.position = new Vector3(transform.position.x, yPosition, transform.position.z);
+
+	}
+}
