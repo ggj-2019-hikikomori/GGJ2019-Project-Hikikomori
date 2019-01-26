1d3397da832ae79d5ddada3c562eae5ef4b7ec5a
Eva <eva.gerbert.gaillard@gmail.com>
2019-01-26 09:08:34 +0100

    dialog house setup

---
+++ Assets/Scripts/Dialog/DialogHouseSetup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogHouseSetup : MonoBehaviour {
+
+    public DialogWithPlayer goingOutside;
+
+	void Start () {
+        if (!GameManager.instance.spawnOnBed)
+            Destroy(goingOutside.gameObject);
+    }
+}
