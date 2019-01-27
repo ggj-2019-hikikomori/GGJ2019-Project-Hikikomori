f2258bee8031287537b14082c0bd3b3a0ad8de56
Desages NATHAN <nathan.desages@gmail.com>
2019-01-27 16:29:14 +0100

    added script finish setup bakey pickup

--- Assets/Scripts/bakeryPickable.cs
+++ Assets/Scripts/bakeryPickable.cs
@@ -4,37 +4,13 @@ using UnityEngine;
 
 public class bakeryPickable : MonoBehaviour
 {
-	public GameObject jam;
-	public GameObject smallFlour;
-	public GameObject bigFlour;
-
 	public PickManager picker;
 
-	public Sprite flourSprite;
-	public Sprite jamSprite;
-	public Sprite seedSprite;
+	public Sprite baguetteSprite;
 
 	void Update()
 	{
-		if (GameManager.GetVariable("item_jam") == 1)
-			jam.SetActive(true);
-		if (GameManager.GetVariable("item_flour_small") == 1)
-		{
-			picker.SetSlot(flourSprite);
-			Destroy(smallFlour);
-			bigFlour.GetComponent<GlowObjectCmd>().glowActive = false;
-		}
-		if (GameManager.GetVariable("item_flour_big") == 1)
-		{
-			picker.SetSlot(flourSprite);
-			Destroy(bigFlour);
-			smallFlour.GetComponent<GlowObjectCmd>().glowActive = false;
-		}
-		if (GameManager.GetVariable("item_seeds") == 1)
-		{
-			picker.EmptySlot(jamSprite);
-			picker.SetSlot(seedSprite);
-		}
-
+		if (GameManager.GetVariable("item_baguette") == 1)
+			picker.SetSlot(baguetteSprite);
 	}
 }
